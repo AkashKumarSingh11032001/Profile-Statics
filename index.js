1c94d24e733a4182f3389a7032f5c2e9bade1000
Akash <akashkumarsingh11032001@gmail.com>
2022-08-14 17:08:33 +0530

    js func.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const countersEl = document.querySelectorAll(".counter")
 
 countersEl.forEach((x)=>{
-    x.innerText = "0";
+    x.innerHTML = "0";
+    incrementCounter();
+    function incrementCounter(){
+        let currNum = +x.innerText;
+        const ceilNum = x.getAttribute("data-ceil")
+    }
 });
