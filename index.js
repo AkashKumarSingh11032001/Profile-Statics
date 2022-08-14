248d73ea7854fc04ded9108f13b172e98fa84eff
Akash <akashkumarsingh11032001@gmail.com>
2022-08-14 17:11:40 +0530

    js func.

--- index.js
+++ index.js
@@ -6,5 +6,11 @@ countersEl.forEach((x)=>{
     function incrementCounter(){
         let currNum = +x.innerText;
         const ceilNum = x.getAttribute("data-ceil")
+        const increment = ceilNum / 15;
+        currNum = Math.ceil(currNum + increment);
+        x.innerText = currNum;
+        if(currNum < ceilNum){
+            setTimeout(incrementCounter,50);
+        }
     }
 });
