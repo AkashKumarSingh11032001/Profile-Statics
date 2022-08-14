7675d6c525de416897dd46023af23a02cf42ed0e
Akash <akashkumarsingh11032001@gmail.com>
2022-08-14 17:15:19 +0530

    proj 2 done

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const countersEl = document.querySelectorAll(".counter")
 
 countersEl.forEach((x)=>{
-    x.innerHTML = "0";
+    x.innerText = "0";
     incrementCounter();
     function incrementCounter(){
         let currNum = +x.innerText;
         const ceilNum = x.getAttribute("data-ceil")
         const increment = ceilNum / 15;
         currNum = Math.ceil(currNum + increment);
-        x.innerText = currNum;
+        
         if(currNum < ceilNum){
+            x.innerText = currNum;
             setTimeout(incrementCounter,50);
+        }else{
+            x.innerText = ceilNum;
         }
     }
 });
